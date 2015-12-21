3515ff55dcce6d38382bea5708ca611307280332
Harrisson- <steven.clerc@epitech.eu>
2015-12-21 23:53:05 +0900

    client connect

--- Client.pl
+++ Client.pl
@@ -6,3 +6,10 @@ use MyLib;
 my $port = get_port();
 my $ip = get_ip();
 my $sock = socket_client_init($ip, $port);
+
+my $mess = <STDIN>;
+$sock->send($mess);
+shutdown($sock, 1);
+$sock->recv(my $rep, 1024);
+print("$rep");
+$sock->close();
